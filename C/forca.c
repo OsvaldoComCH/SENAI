8778b83c6b70ce1105a94917c92373ccc0d0fded
Analiovilsson <146580680+Analiovilsson@users.noreply.github.com>
2024-03-26 16:15:05 -0300

    Add files via upload

--- C/forca.c
+++ C/forca.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
 #include <Windows.h>
+#include <string.h>
 char Palavra[2][11] = {"radiologia", "----------"};
-int CheckLetter(char Letter)
+int Tentativas = 6;
+char Letra;
+int CheckLetter()
 {
+    int ret = 0;
     for(int i = 0; i < 11; ++i)
     {
-        if(Letter % 32 + 96)
+        if(!((Letra | 32) ^ Palavra[0][i]))
         {
             Palavra[1][i] = Palavra[0][i];
+            ++ret;
         }
     }
+    return ret;
+}
+void Input()
+{
+    system("cls");
+    printf("Tu tens %i tentativas\n[ %s ]\n\nUma letra: ", Tentativas, Palavra[1]);
+    scanf(" %c", &Letra);
 }
 int main()
 {
-
+    while(Tentativas)
+    {
+        Input();
+        if(CheckLetter())
+        {
+            if(!strcmp(Palavra[0], Palavra[1]))
+            {
+                break;
+            }
+        }else
+        {
+            --Tentativas;
+        }
+    }
+    system("cls");
+    printf("%s\n[ %s ]\n", Tentativas ? "Acertou!":"Errado, a palavra correta era:", Palavra[0]);
 }
