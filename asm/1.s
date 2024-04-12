5d7a4dd05e80d7fa0cc9aa3a0baf46f85e302cdd
Analiovilsson <146580680+Analiovilsson@users.noreply.github.com>
2024-04-12 11:26:31 -0300

    Add files via upload

--- asm/1.s
+++ asm/1.s
@@ -1,14 +1,26 @@
 .section .rdata
+l1:
+	.ascii "%i\n"
+l2:
+	.ascii "%i "
+.section .data
 x:
-    .ascii  "Bom Dia"
-.text
+	.long 0
+.section .text
 .globl main
+.globl printnumber
+printnumber:
+	test	4(%esp), 4(%esp)
+	jz		pn0
+	pushl	$l1
+	jmp		pn1
+pn0:
+	pushl	$l2
+pn1:
+	call	_printf
+	addl	$4, %esp
+	ret
 main:
-    pushl	$-11
-    call    _GetStdHandle@4
-    pushl	$0
-    pushl	$0
-    pushl   $7
-    pushl	$x
-    pushl	%eax
-    call    _WriteConsoleA@20
+	pushl	$1
+	pushl	$10
+	call	printnumber
