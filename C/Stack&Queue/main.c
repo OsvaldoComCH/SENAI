b74a73ed2e954accd93ad4efa02a50c6342448da
Eduardo Henrique Ribeiro da Silva <146580680+Analiovilsson@users.noreply.github.com>
2024-05-16 16:49:27 -0300

    Add files via upload

--- C/Stack&Queue/main.c
+++ C/Stack&Queue/main.c
@@ -1,37 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Stack.h"
-#include "Queue.h"
+#include <time.h>
+#include "Stack&Queue.h"
+
+int Fila(QS * Queue)
+{
+    int id = 0;
+    int Time = 0; //0 = 8:00, 1 = 8:05, 108 = 17:00, 168 = 22:00
+    while(Time++ < 168)
+    {
+        if(!(rand() % (2 + (Time < 108) * 3)))
+        {
+            Enqueue(Queue, ++id);
+            printf("Pessoa %i entrou: %i:%i\n", id, 8 + (Time / 12), 5*(Time % 12));
+        }
+        if(!(rand() % 10) && QSSize(Queue) > 0)
+        {
+            printf("Pessoa %i saiu: %i:%i\n", Dequeue(Queue), 8 + (Time / 12), 5*(Time % 12));
+            if(QSSize(Queue) <= 0)
+            {
+                printf("Acabou a fila.");
+                return 0;
+            }
+        }
+        for(int i = 0; i < QSSize(Queue); ++i)
+        {
+            if(!(rand() % 100))
+            {
+                printf("Pessoa %i desistiu\n", Remove(Queue, i));
+                if(QSSize(Queue) <= 0)
+                {
+                    printf("Acabou a fila.");
+                    return 0;
+                }
+            }
+        }
+    }
+}
 
 int main()
 {
-    STACK S;
-    QUEUE Q;
-    CreateStack(&S, 10);
-    StackPush(&S, 1);
-    StackPush(&S, 2);
-    StackPush(&S, 3);
-    StackPush(&S, 4);
-    StackPush(&S, 5);
-    for(int i = 0; i < 5; ++i)
-    {
-        printf("%i,", StackPop(&S));
-    }
-    /*printf("%i, %i, %i, %i, %i", StackPop(&S), StackPop(&S),
-    StackPop(&S), StackPop(&S), StackPop(&S));*/
-    DestroyStack(&S);
-
-    CreateQueue(&Q, 10);
-    Enqueue(&Q, 1);
-    Enqueue(&Q, 2);
-    Enqueue(&Q, 3);
-    Enqueue(&Q, 4);
-    Enqueue(&Q, 5);
-    for(int i = 0; i < 5; ++i)
-    {
-        printf("%i,", Dequeue(&Q));
-    }
-    /*printf("%i, %i, %i, %i, %i", Dequeue(&Q), Dequeue(&Q),
-    Dequeue(&Q), Dequeue(&Q), Dequeue(&Q));*/
-    DestroyQueue(&Q);
+    srand(time(NULL));
+    QUEUE Queue;
+    CreateQueue(&Queue, 100);
+    Fila(&Queue);
+    DestroyQueue(&Queue);
 }
