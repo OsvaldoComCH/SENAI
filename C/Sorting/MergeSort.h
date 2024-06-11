6abebdfb64ded1555697e9edfc9306b1a8414199
Eduardo Henrique Ribeiro da Silva <146580680+OsvaldoComCH@users.noreply.github.com>
2024-06-11 15:52:22 -0300

    Add files via upload

--- C/Sorting/MergeSort.h
+++ C/Sorting/MergeSort.h
@@ -1,17 +1,44 @@
 #ifndef MERGE_SORT
 #define MERGE_SORT
+#include <stdlib.h>
 
-void InsertionSort(int * A, int Size)
+void MergeSort(int * A, int Start, int End)
 {
-    for(int i = 1, j = 0; i < Size; j = i, ++i)
+    if(Start >= End)
     {
-        int x = A[i];
-        while(j >= 0 && A[j] > x)
-        {
-            A[j+1] = A[j];
-            --j;
-        }
-        A[j+1] = x;
+        return;
     }
+
+    int Mid = Start+((End-Start) >> 1);
+    MergeSort(A, Start, Mid);
+    MergeSort(A, Mid + 1, End);
+
+    int * B = (int *) malloc((End-Start+1)*sizeof(int));
+    int x = Start, y = Mid+1, z = 0;
+
+    while(x <= Mid && y <= End)
+    {
+        if(A[x] < A[y])
+        {
+            B[z++] = A[x++];
+        }else
+        {
+            B[z++] = A[y++];
+        }
+    }
+    while(x <= Mid)
+    {
+        B[z++] = A[x++];
+    }
+    while(y <= End)
+    {
+        B[z++] = A[y++];
+    }
+
+    for(int i = Start, j = 0; i <= End; ++i, ++j)
+    {
+        A[i] = B[j];
+    }
+    free(B);
 }
 #endif
